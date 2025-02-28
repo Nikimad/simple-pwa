060a454ef80b21bc646cf0866b7e2d1bcb5d1631
Nikimad <PybePoiD@gmail.com>
2025-02-28 15:28:09 +0300

    Add src/components/SubscriptionPlans

---
+++ src/components/SubscriptionPlans/SubscriptionPlans.tsx
@@ -0,0 +1,27 @@
+import { SubscriptionOptionsEnum } from "@/types/subscription";
+import { subscriptionOptions } from "@/constants/subscription";
+import FormHeader from "../FormHeader";
+import SubscriptionPlan from "../SubscriptionOption";
+import SubscriptionType from "../SubscriptionType";
+import s from "./SubscriptionPlans.module.scss";
+
+const SubscriptionPlans = () => (
+  <>
+    <FormHeader
+      title="Select ypur plan"
+      description="You have the option of monthly or yearly billing."
+    />
+    <div className={s.subscriptionplans__container}>
+      <ul className={s.subscriptionplans}>
+        {(Object.keys(subscriptionOptions) as SubscriptionOptionsEnum[]).map(
+          (option) => (
+            <SubscriptionPlan key={option} option={option} />
+          )
+        )}
+      </ul>
+      <SubscriptionType />
+    </div>
+  </>
+);
+
+export default SubscriptionPlans;
